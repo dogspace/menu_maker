0a3ced7bcf374015ffdc52371a65d0674d5f1865
dogspace <43654839+dogspace@users.noreply.github.com>
2023-09-18 20:47:25 +0000

    Site layout, basic JS

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,33 +3,55 @@ document.addEventListener('DOMContentLoaded', function() {
     var s,
     MenuMaker = {
         settings: {
-            'x': 0,
-            'tableNames': [],
-
             'groupSelector': document.querySelector('.group-selector'),
             'groupDropdown': document.querySelector('.group-dropdown'),
             'groupItems': document.querySelectorAll('.group-item'),
+            'groupRenameButtons': document.querySelectorAll('.group-rename'),
 
         },
 
-        //
+        // Site init
         init: function() {
             console.log('>> >> >> >> >> >>>>   init   <<<< << << << << <<')
             s = MenuMaker.settings
             MenuMaker.bindUIActions()
         },
 
+        // Bind UI actions
         bindUIActions: function() {
             console.log(">> bindUIActions <<")
-
             s.groupSelector.addEventListener('click', MenuMaker.toggleGroupDropdown)
+            s.groupRenameButtons.forEach(button => { button.addEventListener('click', MenuMaker.renameGroup) })
+
         },
 
-
+        // Toggle dropdown menu, assign temporary listener if opened
         toggleGroupDropdown: function() {
+            console.log(">> toggleGroupDropdown")
             s.groupDropdown.classList.toggle('hidden')
-
+            if (!s.groupDropdown.classList.contains('hidden')) {
+                document.addEventListener('click', MenuMaker.checkDropdownClick, true)
+            } else {
+                document.removeEventListener('click', MenuMaker.checkDropdownClick, true)
+            }
         },
+
+        // Listener, runs while dropdown is open, ends when user clicks outside of dropdown
+        checkDropdownClick: function(event) {
+            console.log(">> checkClick")
+            let target = event.target
+            if (!s.groupSelector.contains(target) && !s.groupDropdown.contains(target)) {
+                document.removeEventListener('click', MenuMaker.checkDropdownClick, true)
+                MenuMaker.toggleGroupDropdown()
+            }
+        },
+
+        // //
+        // renameGroup: function(event) {
+        //     let button = event.target.closest('.group-rename')
+        //     let groupName = button.previousElementSibling
+        //     // UNFINISHED
+        // },
 
 
 
